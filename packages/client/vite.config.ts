5b86b24446306b5aed6a726d2d26add25173b82c
Frozen FIsh <976499226@qq.com>
2022-07-26 23:36:34 +0800

    chore: update vite.confg.ts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Inspect from 'vite-plugin-inspect'
-import HotExport from 'vite-plugin-hot-export'
+import { vitePlugin as HotExport } from 'unplugin-hot-export'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
