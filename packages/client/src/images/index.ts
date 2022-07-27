bf1fe4c378113a860573e0ac3a8ad6d6fd24b8aa
Frozen FIsh <976499226@qq.com>
2022-07-27 17:34:49 +0800

    chore: update index.ts

--- packages/client/src/images/index.ts
+++ packages/client/src/images/index.ts
@@ -1,5 +1,11 @@
+import JpegDoodleKawai from './doodle kawai.jpeg'
+import PngIcon from './icon.png'
+import PngScreenshot from './screenshot.png'
 import PngWebstorm from './webstorm.png'
 
 export {
+  JpegDoodleKawai,
+  PngIcon,
+  PngScreenshot,
   PngWebstorm
 }
